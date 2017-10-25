52fb07afe493efb8589cd2783fa67587c5ce38d7
Firas Sfaxi <firas@mill.ly>
2017-10-25 10:21:16 +0200

    Add project files.

---
+++ PMContext.cs
@@ -0,0 +1,18 @@
+﻿/*
+using System;
+using Microsoft.EntityFrameworkCore;
+using PackageManagment.Domain;
+
+namespace PackageManagment.Infra
+{
+    public class PMContext
+    {
+        public PMContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
+*/
